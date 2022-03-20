339813d39888b6d44f9955b7a657c664c1b6dc36
tasmankingsley <tasman.kingsley@gmail.com>
2022-03-20 21:00:24 +1100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+import Word from './lib/Word.svelte';
+
+let words = [];
+
+for (let index = 0; index < 500; index++) {
+  words[index] = {word: index, description: 'jhkhsoie clkhsowdn'};
+}
+</script>
+
+
+<div class="header">Japanese Jisho</div>
+<div class="grid">
+  {#each words as word}
+    <svelte:component this={Word}
+      word={word.word}
+      description={word.description}
+    />
+  {/each}
+</div>
+
+
+<style>
+.header {
+  font-size: 1.5rem;
+  padding: 5px;
+  height: 30px;
+  position: sticky;
+  top: 0;
+  background-color: black;
+  text-align: center;
+}
+.grid {
+  display: grid;
+  grid-auto-flow: row;
+  color: black;
+}
+</style>
---
+++ src/lib/Word.svelte
@@ -0,0 +1,23 @@
+<script>
+
+export let word;
+export let description;
+
+</script>
+
+<div class="word">
+    <span>{word}</span>
+    <span>{description}</span>
+</div>
+
+<style>
+.word {
+    background-color: #bbb;
+    height: 60px;
+    line-height: 60px;
+    border-bottom: 2px solid #333;
+    text-align: left;
+    padding-left: 10px;
+}
+ 
+</style>
